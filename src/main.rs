314c93c9f8347d8bfbfeb9bd57b0dc5fa6e21a2f
artemenko_pa <artemenko_pa@magnit.ru>
2022-01-18 16:00:39 +0300

    Generic version

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,82 @@
+use std::collections::HashMap;
 use std::ops::Deref;
 use std::sync::Arc;
 use std::thread;
-use std::time::Duration;
+use std::thread::Thread;
+use std::time::{Duration, Instant};
+use parking_lot::{Mutex, RwLock};
 
 
-use concurrent_lru::sharded::LruCache;
 use rand::Rng;
 use rand::rngs::ThreadRng;
 
 #[derive(Debug)]
 struct CacheEntry {
-    id: usize,
     data: Vec<Vec<String>>,
 }
 
 impl Drop for CacheEntry {
     fn drop(&mut self) {
-        println!("Cube {} is evicted", self.id);
+        println!("Cube: {:?} dropped", self);
     }
 }
 
 struct CacheManager {
-    cache: LruCache<usize, Arc<CacheEntry>>,
+    max_size: usize,
+    cache: RwLock<HashMap<usize, Arc<CacheEntry>>>,
+    last_access: RwLock<HashMap<usize, Instant>>,
+    entity_locks: RwLock<HashMap<usize, Arc<Mutex<i8>>>>,
 }
 
 impl CacheManager {
     pub fn new(size: usize) -> Self {
         Self {
-            cache: LruCache::new(size as u64),
+            max_size: size,
+            cache: RwLock::new(HashMap::new()),
+            last_access: RwLock::new(HashMap::new()),
+            entity_locks: RwLock::new(HashMap::new()),
         }
     }
 
     pub fn get_entry(&self, id: usize) -> Arc<CacheEntry> {
-        self.cache.get_or_init(id, 1, |entry_id| { CacheManager::get_data(*entry_id) });
-        while self.cache.get(id).is_none() {
-            thread::sleep(Duration::from_millis(1));
-        }
-        self.cache.get(id).unwrap().value().clone()
-    }
+        let mut guard = self.cache.write();
+        self.last_access.write().insert(id, Instant::now());
+        let mut entry_mutex: Arc<Mutex<i8>>;
+        if guard.contains_key(&id) {
+            return guard.get(&id).unwrap().clone();
+        } else {
+            while guard.len() >= self.max_size {
+                let mut earliest_instant = Instant::now();
+                let mut earliest_id = usize::MAX;
+                for item in self.last_access.read().iter() {
+                    if earliest_instant > *item.1 {
+                        earliest_id = *item.0;
+                        earliest_instant = *item.1;
+                    }
+                }
+                guard.remove(&earliest_id);
+                self.last_access.write().remove(&earliest_id);
+                self.entity_locks.write().remove(&earliest_id);
+            }
 
-    fn get_data(id: usize) -> Arc<CacheEntry> {
-        println!("Loading data for cube: {}", id);
-        Arc::new(CacheEntry { id, data: vec![vec![format!("Data for cube with id: {}", id)]]})
+            if !self.entity_locks.read().contains_key(&id) {
+                self.entity_locks.write().insert(id, Arc::new(Mutex::new(0i8)));
+            }
+            let tmp_lock = self.entity_locks.read();
+            entry_mutex = tmp_lock.get(&id).unwrap().clone();
+        }
+        drop(guard);
+        let mut guard = entry_mutex.lock();
+        *guard = 0;
+        if self.cache.read().contains_key(&id) {
+            return self.cache.read().get(&id).unwrap().clone();
+        } else {
+            self.cache.write().insert(id, Arc::new(CacheEntry { data: vec![vec![format!("Data for cube {}", id)]]}));
+        }
+        let entry = self.cache.read().get(&id).unwrap().clone();
+        *guard = 1;
+        drop(guard);
+        entry
     }
 }
 
@@ -56,7 +91,7 @@ fn main() {
             let mut rng = ThreadRng::default();
             let duration = rng.gen_range(10..=500);
             thread::sleep(Duration::from_millis(duration));
-            let entry_id = rng.gen_range(0..20);
+            let entry_id = rng.gen_range(0..30);
             //println!("Thread {} requested entry {}", i, entry_id);
             let cache_entry = manager.deref().get_entry(entry_id);
             println!("Data in thread number: {}, is: {:?}", i, cache_entry);
